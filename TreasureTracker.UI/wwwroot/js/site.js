9e1f5ade03727faf26195ae20fcb586ec7b3383d
Khurshid Yuldoshev <xurshidyoldoshev4@gmail.com>
2024-03-14 09:12:52 +0500

    Dark mode added

--- TreasureTracker.UI/wwwroot/js/site.js
+++ TreasureTracker.UI/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const body = document.querySelector('body');
+const btn = document.querySelector('.btn');
+const icon = document.querySelector('.btn__icon');
 
-// Write your JavaScript code.
+//to save the dark mode use the object "local storage".
+
+//function that stores the value true if the dark mode is activated or false if it's not.
+function store(value) {
+    localStorage.setItem('darkmode', value);
+}
+
+//function that indicates if the "darkmode" property exists. It loads the page as we had left it.
+function load() {
+    const darkmode = localStorage.getItem('darkmode');
+
+    //if the dark mode was never activated
+    if (!darkmode) {
+        store(false);
+        icon.classList.add('fa-sun');
+    }
+    else if (darkmode == 'true') { //if the dark mode is activated
+        body.classList.add('darkmode');
+        icon.classList.add('fa-moon');
+    }
+    else if (darkmode == 'false') { //if the dark mode exists but is disabled
+        icon.classList.add('fa-sun');
+    }
+}
+
+
+load();
+
+btn.addEventListener('click', () => {
+
+    body.classList.toggle('darkmode');
+    icon.classList.add('animated');
+
+    //save true or false
+    store(body.classList.contains('darkmode'));
+
+    if (body.classList.contains('darkmode')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+    else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+
+    setTimeout(() => {
+        icon.classList.remove('animated');
+    }, 500)
+})
