6fdaacb9c5de26b3b235a9fcc3bf1ebf3d5c756a
Khurshid Yuldoshev <xurshidyoldoshev4@gmail.com>
2024-03-25 16:37:55 +0500

    Repositories added.

---
+++ TreasureTracker.Data/Repositories/CollectionRepository.cs
@@ -0,0 +1,12 @@
+﻿using TreasureTracker.Data.Db;
+using TreasureTracker.Domain.Entities;
+using TreasureTracker.Data.IRepositories;
+using TreasureTracker.Domain.Repositories;
+
+namespace TreasureTracker.Data.Repositories;
+public class CollectionRepository : Repository<Collection>, ICollectionRepository
+{
+    public CollectionRepository(DataContext context) : base(context)
+    {
+    }
+}
---
+++ TreasureTracker.Data/Repositories/ItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using TreasureTracker.Data.Db;
+using TreasureTracker.Domain.Entities;
+using TreasureTracker.Data.IRepositories;
+using TreasureTracker.Domain.Repositories;
+
+namespace TreasureTracker.Data.Repositories;
+public class ItemRepository : Repository<Item>, IItemRepository
+{
+    public ItemRepository(DataContext context) : base(context)
+    {
+    }
+}
