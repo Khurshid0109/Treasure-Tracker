f8bb5cc09f3e6566790c2ec88fe921486c706b5d
Khurshid Yuldoshev <xurshidyoldoshev4@gmail.com>
2024-03-29 07:37:48 +0500

    Validations inui.

---
+++ TreasureTracker.Service/DTOs/Auth/ExistEmailPostModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TreasureTracker.Service.Helpers;
+
+namespace TreasureTracker.Service.DTOs.Auth;
+public class ExistEmailPostModel
+{
+    [Required(ErrorMessage ="Email is required")]
+    [TTrackerEmailAttribute(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; }
+}
